8199f731e1cde1d5a8e7c336bb245d610229fdb5
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-06-26 17:58:48 +0200

    working on popular recipes

--- src/components/Main_Components/AppMain.vue
+++ src/components/Main_Components/AppMain.vue
@@ -1,15 +1,18 @@
 <template>
   <AppJumbotron/>
   <AppFoodieJournal/>
+  <AppPopularRecipes/>
   </template>
   <script>
   import AppJumbotron from '../Main_Components/AppJumbotron.vue';
   import AppFoodieJournal from './AppFoodieJournal.vue';
+  import AppPopularRecipes from './AppPopularRecipes.vue';
   export default {
     name:"AppMain",
     components:{
       AppJumbotron,
       AppFoodieJournal,
+      AppPopularRecipes
     }
   }
   </script>
---
+++ src/components/Main_Components/AppPopularRecipes.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h4 class="text-center">
+            POPULAR RECIPES
+        </h4>
+        <p class="text-center">
+            Aliquam erat volutpat. Ut quis ligula a magna blandit finibus. Suspendisse maximus lacus non nunc lacinia lobortis.
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+@use '../../styles/partials/mixin' as *; 
+@use '../../styles/partials/variables' as *;
+
+h4{
+    font-weight: 200;
+    font-size: 1rem;
+    margin-bottom: 1.5rem;
+}
+p{
+    padding: 0 28rem;
+    font-weight: 200;
+    color: $darkGrey;
+}
+</style>
