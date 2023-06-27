320c309b3fbfbdb44f5f0da36b2a99b4e0d87998
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-06-27 10:28:50 +0200

    working recipes

--- src/components/Main_Components/AppRecipes.vue
+++ src/components/Main_Components/AppRecipes.vue
@@ -1,24 +1,39 @@
 <template>
     <div class="container">
-        <div class="row card-dish-big">
-            <div class="col-6 px-4">
-                <img :src="imageRecipe" alt="">
+        <div class="row">
+            <div class="col-6 card-dish-big">
+                <img class="img-card" :src="getImagePath(imageRecipe)" alt="">
                 <h4>
                     {{ titleRecipe }}
                 </h4>
                 <p class="description mt-3">
                     {{ descriptionRecipe }}
                 </p>
-                {{ dishRecipe }}
+                <div class="dotted-line my-4">
+                </div>
+                <p>
+                    {{ dishRecipe }}
+                </p>            
                 <button>
                     {{buttonRecipe}}
                 </button>
+            </div>
+            <div class="col-3 px-4 card-dish-big">
+                <img :src="imageRecipe" alt="">
+            </div>
+            <div class="col-3 px-4 card-dish-big">
+                <img :src="imageRecipe" alt="">
             </div>
         </div>
     </div>
 </template>
 <script>
 export default {
+    methods: {
+        getImagePath: function(img) {
+        return new URL(`../assets/img/${img}`, import.meta.url).href;
+}
+    },
 
     props:{
         imageRecipe:String,
@@ -47,9 +62,13 @@ export default {
         padding: 0.5rem 2.5rem;
         color: $white;
         font-weight: 200;
-        border-radius: 0.5rem;
+        border-radius: 0.15rem;
         font-size: 0.9rem;
         border: none;
+    }
+    .dotted-line{
+        width: 100%;
+        border: 1px dotted $dottedColor;
     }
 }
 </style>
