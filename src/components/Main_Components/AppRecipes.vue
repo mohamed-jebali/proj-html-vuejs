f5adc18fead5e46bc46d3b67e2f94d0f457ddc25
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-06-27 15:40:18 +0200

    added recipes

--- src/components/Main_Components/AppRecipes.vue
+++ src/components/Main_Components/AppRecipes.vue
@@ -23,29 +23,29 @@
                 </div>
             </div>
             <div class="container-single-dish d-flex">
-                <div class="single-dish mb-3">
+                <div class="single-dish">
+                    <img src="../../assets/img/Mixed-fruits-1200x790.jpg" alt="rachel-park-dish">
+                </div>
+                <div class="single-dish">
                     <img src="../../assets/img/r-rachel-park-366508-unsplash-min-1200x790.jpg" alt="rachel-park-dish">
                 </div>
-                <div class="single-dish mb-3">
-                    <img src="../../assets/img/r-rachel-park-366508-unsplash-min-1200x790.jpg" alt="rachel-park-dish">
+                <div class="single-dish">
+                    <img src="../../assets/img/r-michelle-tsang-500721-unsplash-min-600x395.jpg" alt="rachel-park-dish">
                 </div>
-                <div class="single-dish mb-3">
-                    <img src="../../assets/img/r-rachel-park-366508-unsplash-min-1200x790.jpg" alt="rachel-park-dish">
+                <div class="single-dish">
+                    <img src="../../assets/img/quick-summer-drink-460x295.jpg" alt="rachel-park-dish">
                 </div>
-                <div class="single-dish mb-3">
-                    <img src="../../assets/img/r-rachel-park-366508-unsplash-min-1200x790.jpg" alt="rachel-park-dish">
+                <div class="single-dish">
+                    <img src="../../assets/img/r-maarten-van-den-heuvel-400626-unsplash-min-460x295.jpg" alt="rachel-park-dish">
                 </div>
-                <div class="single-dish mb-3">
-                    <img src="../../assets/img/r-rachel-park-366508-unsplash-min-1200x790.jpg" alt="rachel-park-dish">
+                <div class="single-dish">
+                    <img src="../../assets/img/perfect-cosmopolitan-460x295.jpg" alt="rachel-park-dish">
                 </div>
-                <div class="single-dish mb-3">
-                    <img src="../../assets/img/r-rachel-park-366508-unsplash-min-1200x790.jpg" alt="rachel-park-dish">
+                <div class="single-dish">
+                    <img src="../../assets/img/fi2x-6-460x295.jpg" alt="rachel-park-dish">
                 </div>
-                <div class="single-dish mb-3">
-                    <img src="../../assets/img/r-rachel-park-366508-unsplash-min-1200x790.jpg" alt="rachel-park-dish">
-                </div>
-                <div class="single-dish mb-3">
-                    <img src="../../assets/img/r-rachel-park-366508-unsplash-min-1200x790.jpg" alt="rachel-park-dish">
+                <div class="single-dish">
+                    <img src="../../assets/img/r-brooke-lark-96398-unsplash-min-460x295.jpg" alt="rachel-park-dish">
                 </div>
             </div>
         </div>
@@ -81,14 +81,21 @@ export default {
     width: calc(100% / 2);
     border-left: 1.5rem solid #f5f5f1;
     height: 100%;
+    margin-bottom: 1.7rem;
     img{
         width: 100%;
+        &:hover{
+            opacity: 0.5;
+            transition: 1s;
+            background-color: radial-gradient(circle farthest-side, #fceabb, #f8b500);
+        }
     }
 }
 .card-dish-big{
     background-color: white;
     box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
     width: calc(100% / 2 - 1rem);
+    height: 100%;
     margin-right: 1rem;
     h4{
         font-size: 1.3rem;
@@ -106,6 +113,7 @@ export default {
         border-radius: 0.15rem;
         font-size: 0.9rem;
         border: none;
+        margin-bottom: 1rem;
     }
     .dotted-line{
         width: 100%;
