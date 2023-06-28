12b50dc5f3d13b98b02ad182b2b1f09350547376
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-06-28 12:28:23 +0200

    added posts

--- src/components/Main_Components/AppPosts.vue
+++ src/components/Main_Components/AppPosts.vue
@@ -1,45 +1,12 @@
 <template>
        <div class="container">
-        <div class="image-wrapper d-flex flex-wrap">
-            <div class="card-dish-social p-0 mb-5">
-                    <div class="img-container">
-                        <img src="../../assets/img/single-post-img3-1200x790.jpg"/>
-                    </div>
-                    <div class="text-card-container text-center">
-                        <h4>
-                            Food Corner: Top Japanese Restaurants for Sushi
-                        </h4>
-                        <p class="description mt-1">
-                            By admin | March 25th, 2019
-                        </p>  
-                    </div>
-            </div>
-            <div class="card-dish-social p-0 mb-5">
-                    <div class="img-container">
-                        <img src="../../assets/img/single-post-img3-1200x790.jpg"/>
-                    </div>
-                    <div class="text-card-container text-center">
-                        <h4>
-                            Food Corner: Top Japanese Restaurants for Sushi
-                        </h4>
-                        <p class="description mt-1">
-                            By admin | March 25th, 2019
-                        </p>  
-                    </div>
-            </div>
-            <div class="card-dish-social p-0 mb-5">
-                    <div class="img-container">
-                        <img src="../../assets/img/single-post-img3-1200x790.jpg"/>
-                    </div>
-                    <div class="text-card-container text-center">
-                        <h4>
-                            Food Corner: Top Japanese Restaurants for Sushi
-                        </h4>
-                        <p class="description mt-1">
-                            By admin | March 25th, 2019
-                        </p>  
-                    </div>
-            </div>
+        <div class="image-wrapper">
+            <AppSinglePost class="d-flex flex-wrap" v-for="post in listPosts"
+            :image="post.image"
+            :title="post.title"
+            :date="post.date"
+            />
+        </div>
    <nav class="navbar bg-body-tertiary">
   <div class="container-fluid">
     <form class="d-flex" role="search">
@@ -48,83 +15,58 @@
   </div>
     </nav>
         </div>
-    </div>
 </template>
 <script>
+import AppSinglePost from './AppSinglePost.vue';
 export default {
-    
+    components:{
+    AppSinglePost
+},
+    data() {
+        return {
+            listPosts:[
+                {
+                    image:'https://avada.website/food/wp-content/uploads/sites/118/2019/03/fi-roundup.jpg',
+                    title:'Roundup: My New Favourite Recipes For Healthy Living',
+                    date:'By admin | March 25th, 2019'
+                },
+                {
+                    image:'https://avada.website/food/wp-content/uploads/sites/118/2019/03/fi-korean-food.jpg',
+                    title:'Meal Prep: Korean Bibimbap with Kimchi',
+                    date:'By admin | March 25th, 2019'
+                },
+                {
+                    image:'https://avada.website/food/wp-content/uploads/sites/118/2019/03/fi-toasts.jpg',
+                    title:'Why These Toasts with Tea are My New Favorite',
+                    date:'By admin | March 25th, 2019'
+                },
+                {
+                    image:'https://avada.website/food/wp-content/uploads/sites/118/2019/03/fi-street-food.jpg',
+                    title:'Exploring Street Food in Bangkok',
+                    date:'By admin | March 25th, 2019'
+                },
+                {
+                    image:'https://avada.website/food/wp-content/uploads/sites/118/2019/03/fi-organic-breakfast.jpg',
+                    title:'Organic Choices For Healthier Living',
+                    date:'By admin | March 25th, 2019'
+                },
+                {
+                    image:'https://avada.website/food/wp-content/uploads/sites/118/2019/03/fi-water-side-rest.jpg',
+                    title:'5 Waterside Restaurants in Istanbul for Special Events',
+                    date:'By admin | March 25th, 2019'
+                },
+            ]
+        }
+    },
 }
 </script>
 <style lang="scss" scoped>
 @use '../../styles/partials/mixin' as *; 
 @use '../../styles/partials/variables' as *;
 
-.card-group{
-    width: 30%;
-    height: 100%;
-    img{
-        width: 100%;
-    }
-}
-.left-side{
-    width: 45%;
-}
-h4{
-    font-weight: 200;
-    font-size: 1rem;
-}
-p{
-    font-weight: 200;
-    color: $darkGrey;
-}
-button{
-        background-color: $orange;
-        padding: 0.8rem 2.5rem;
-        color: $white;
-        font-weight: 200;
-        border-radius: 0.2rem;
-        font-size: 0.9rem;
-        border: none;
-    }
-    .card-dish-social{
-    background-color: white;
-    box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
-    width: calc(35% - 4rem );
-    height: 100%;
-    margin-right: 4rem;
-    h4{
-        font-size: 1.3rem;
-        margin-top: 1rem;
-        font-family: 'Vidaloka', serif;
-    }
-    img{
-        width: 100%;
-        &:hover{
-            opacity: 0.5;
-            transition: 1s;
-            background-color: radial-gradient(circle farthest-side, #fceabb, #f8b500);
-        }
-    }
-    button{
-        background-color: $orange;
-        padding: 0.8rem 2.5rem;
-        color: $white;
-        font-weight: 200;
-        border-radius: 0.15rem;
-        font-size: 0.9rem;
-        border: none;
-        margin-bottom: 1rem;
-    }
-    .description{
-        font-weight: 200;
-    }
-    .dish-recipe{
-        color: $darkGrey;
-        font-weight: 200;
-    }
-    .text-card-container{
-    padding: 1rem 1.5rem;
-}
+.image-wrapper{
+    display: flex;
+    flex-wrap: wrap;
 }
 
 </style>
---
+++ src/components/Main_Components/AppSinglePost.vue
@@ -0,0 +1,61 @@
+<template>
+        <div class="card-dish-social p-0 mb-5">
+                    <div class="img-container">
+                        <img :src="image"/>
+                    </div>
+                    <div class="text-card-container text-center">
+                        <h4>
+                            {{ title }}
+                        </h4>
+                        <p class="description mt-1">
+                            {{ date }}
+                        </p>  
+                    </div>
+        </div>
+</template>
+<script>
+export default {
+    props:{
+        image:String,
+        title:String,
+        date:String
+    }
+}
+</script>
+<style lang="scss" scoped>
+@use '../../styles/partials/mixin' as *; 
+@use '../../styles/partials/variables' as *;
+
+h4{
+    font-weight: 200;
+    font-size: 1rem;
+}
+p{
+    font-weight: 200;
+    color: $darkGrey;
+}
+    .card-dish-social{
+    background-color: white;
+    box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);
+    width: calc(35% - 3rem );
+    height: 400px;
+    margin-right: 3rem;
+    h4{
+        font-size: 1.3rem;
+        margin-top: 1rem;
+        font-family: 'Vidaloka', serif;
+    }
+    img{
+        width: 100%;
+        &:hover{
+            opacity: 0.5;
+            transition: 1s;
+            background-color: radial-gradient(circle farthest-side, #fceabb, #f8b500);
+        }
+    }
+    .text-card-container{
+    padding: 1rem 2.8rem;
+}
+}
+
+</style>
